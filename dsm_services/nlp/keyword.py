108a455294eaba855543e54db459d5fcd637d711
SothanaV <nai6320@gmail.com>
2023-12-17 12:45:14 +0700

    [FIX] inherrite from base

--- dsm_services/nlp/keyword.py
+++ dsm_services/nlp/keyword.py
@@ -1,6 +1,6 @@
 from . import base
 
 class KeywordExtract(base.Base):
-    def __init__(self, service_uri, api_key):
-        super().__init__(service_uri, api_key)
+    def __init__(self, service_uri, api_key, _timeout):
+        super().__init__(service_uri, api_key, _timeout)
         self._type = 'KEYWORD'
