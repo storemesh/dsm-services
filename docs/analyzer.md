76e840fc734b6183707fbdd7aa94240284224b81
SothanaV <nai6320@gmail.com>
2024-07-11 11:52:02 +0700

    [UPDATE] api docs for Analyzer

--- docs/analyzer.md
+++ docs/analyzer.md
@@ -1,5 +1,71 @@
 # Analyzer
-```
+
+- init analyzer instance
+```python
 import dsm_services
 
+analyzer = Analyzer(service_uri="https://<SERVICES_URI>", api_key="<API_KEY>")
+```
+
+- list distric
+```python
+analyzer.list_district()
+```
+
+- get distric detail
+```python
+distric = analyzer.get_district(district_id='1-อำเภอเมืองปทุมธานี')
+distric.display()
+```
+
+- prepair context
+```python
+context = analyzer.prep_context(data={
+    '1.1': 'dfellow',
+    '1.2': 'รับจ้างทำ software',
+    '1.3': '29/1493 คลองสาม คลองหลวง ปทุมธานี',
+    '2': 'ชอบเที่ยว',
+    '3': 'เจ้าของอินดี้',
+    '4': 'ลูกค้าเรื่องมาก',
+    '5': 'ไม่รู้',
+    '6': 'เที่ยวไปโค้ดไปกินเวลาวันหยุดไป',
+    '7': 'กลุ่มที่มีเงิน',
+    '8': 'ทุกคนอยากมีตัง',
+    '9': 'ทำไปเรื่อยๆหาเงินเรื่อยๆ',
+    '10': 'ปวดหลังมาก'
+})
+context.display()
+```
+
+- upload context
+```python
+analyzer.upload_context(context=context, district_id="<DISTRIC_ID>")
+```
+
+- get wizmap
+```python
+analyzer.get_wizmap(version=-1)
+```
+
+- RAG
+```python
+analyzer.rag("<PROMPT>").display()
+# example : analyzer.rag("สถานที่ท่องเที่ยวของปทุมธานีคืออะไรบ้าง").display()
+```
+
+- list RAG history
+```python
+analyzer.list_rag()
+```
+
+- get rag history
+```python
+analyzer.get_rag(rag_id=<RAG_ID>).display()
+```
+
+- create mindmap
+```python
+analyzer.create_mindmap("<PROMPT>")
+
+# example : analyzer.create_mindmap("please create mindmap about ปทุมธานี")
 ```
--- dsm_services/analyzer/analyzer.py
+++ dsm_services/analyzer/analyzer.py
@@ -37,6 +37,12 @@ class MarkdownText:
     
 class Analyzer:
     def __init__(self, service_uri, api_key):
+        """Init Analizer class
+
+        Args:
+            service_uri (str): services uri, eg : "https://training.services.storemesh.com"
+            api_key (str): api key, eg : "Bq9EYgko.EBH64MH8LTxXzSPXU8NMKnguEZCHTUGl"
+        """
         self._service_uri = service_uri
         self._header = {
             'Authorization': f'Api-Key {api_key}'
@@ -46,16 +52,38 @@ class Analyzer:
         os.makedirs('tmp', exist_ok=True)
     
     def list_district(self):
+        """List districs
+
+        Returns:
+            List of distric
+        """
         res = requests.get(f"{self._service_uri}/training/api/district/")
         utils.check_http_status_code(response=res)
         return res.json()
 
     def get_district(self, district_id):
+        """get_district detail
+
+        Args:
+            district_id (str): distric id, can get from .list_district()
+
+        Returns:
+            MarkdownText: Markdown display, can call .display() for pretty print
+        """
         res = requests.get(f"{self._service_uri}/training/api/district/{district_id}/")
         utils.check_http_status_code(response=res)
         return MarkdownText(res.json().get('context'))
 
     def prep_context(self, data):
+        """
+        prepair context
+
+        Args:
+            data (dict): has keys {'1.1', '1.2', '1.3', '2', '3', '4', '5', '6', '7', '8', '9'}
+
+        Returns:
+            MarkdownText: Markdown display, can call .display() for pretty print
+        """
         require_key = {'1.1', '1.2', '1.3', '2', '3', '4', '5', '6', '7', '8', '9'}
         diff = require_key - set(data.keys())
         if diff!=set():
@@ -100,6 +128,17 @@ class Analyzer:
         return MarkdownText(template)
             
     def upload_context(self, context, district_id):
+        """upload context to services
+
+        Args:
+            context (MarkdownText): MarkdownText from .prep_context()
+            district_id (str): distric_id from .list_district()
+
+        Raises:
+            Exception: "Please input `context` type MarkdownText"
+        Return:
+            data: context_id and msg
+        """
         if type(context) != MarkdownText:
             raise Exception("Please input `context` type MarkdownText")
         res = requests.post(
@@ -111,8 +150,14 @@ class Analyzer:
        )
         utils.check_http_status_code(response=res)
         return res.json()
-
+        
     def get_wizmap(self, version=-1):
+        """get_wizmap to get wizmap vizualization
+        Args:
+            version (int): version wizmap -1 -> latest 0 -> oldest
+        Return:
+            Iframe display on notebook
+        """
         res = requests.get(f"{self._service_uri}/training/api/wizmap/", headers=self._header)
         utils.check_http_status_code(response=res)
         versions = res.json()[::-1]
@@ -133,16 +178,37 @@ class Analyzer:
         return IFrame(src=url, width='100%', height='600px')
 
     def rag(self, prompt):
+        """RAG promt to return context from llm
+
+        Args:
+            prompt (str): LLM prompt
+
+        Returns:
+            MarkdownText: markdown context contain llm response
+        """
         res = requests.post(f"{self._service_uri}/training/api/rag/", headers=self._header, json={'prompt': prompt})
         utils.check_http_status_code(response=res)
         return MarkdownText(res.json().get('response'))
 
     def list_rag(self):
+        """list history of call LLM
+
+        Returns:
+            MarkdownText: markdown context contain llm response
+        """
         res = requests.get(f"{self._service_uri}/training/api/rag/", headers=self._header)
         utils.check_http_status_code(response=res)
         return res.json()
 
     def get_rag(self, rag_id):
+        """get RAG detail from history
+
+        Args:
+            rag_id (int): rag id from .list_rag()
+
+        Returns:
+            MarkdownText: markdown context contain llm response
+        """
         res = requests.get(f"{self._service_uri}/training/api/rag/{rag_id}/", headers=self._header)
         utils.check_http_status_code(response=res)
         template = f"""
@@ -154,7 +220,16 @@ class Analyzer:
         """
         return MarkdownText(template)
 
-    def create_mindmap(self, prompt):
+    def create_mindmap(self, prompt, colab=False):
+        """create mindmap using llm in mermaid syntax
+
+        Args:
+            prompt (str): LLM prmpt
+            colab (bool, optional): if use in colab please set to `True`. Defaults to False.
+
+        Returns:
+            _type_: _description_
+        """
         res = requests.post(f"{self._service_uri}/training/api/rag/mermaid/", headers=self._header, json={'prompt': prompt})
         utils.check_http_status_code(response=res)
         
@@ -163,5 +238,20 @@ class Analyzer:
         html_file = f"tmp/{uuid.uuid4().hex}.html"
         with open(html_file, "w") as file:
             file.write(html_content)
-        
-        return IFrame(src=html_file, width="100%", height=600), html_file
+        if colab:
+          from google.colab import files
+          return files.download(html_file)
+        return IFrame(src=html_file, width="100%", height=600)
+
+    def llm(self, prompt):
+        """LLM direct call
+
+        Args:
+            prompt (str): LLM prompt
+
+        Returns:
+             MarkdownText: markdown context contain llm response
+        """
+        res = requests.post(f"{self._service_uri}/training/api/rag/", headers=self._header, json={'prompt': prompt})
+        utils.check_http_status_code(response=res)
+        return MarkdownText(res.json().get('response'))
